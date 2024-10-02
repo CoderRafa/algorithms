c2856f2270a5f0c21a7cef36f6d9819d5f9ca5a8
k.iakimchuk <k.yakimchuk@ventra.ru>
2024-10-02 18:46:41 +0300

    Add dictionary functionality

---
+++ src/main/kotlin/com/rafengimprove/practice/model/dictionary/Dictionary.kt
@@ -0,0 +1,21 @@
+package com.rafengimprove.practice.model.dictionary
+
+interface Dictionary<T> {
+    fun search(query: String): T
+    fun search(predicate: (T)->Boolean): List<T>
+}
+
+
+abstract class AbstractDictionaryImpl<T>(val list: MutableList<T>) : Dictionary<T>, MutableList<T> by list {
+    override fun search(predicate: (T) -> Boolean): List<T> {
+        return list.filter { predicate(it) }
+    }
+}
+
+class TelephoneDictionary(list: MutableList<Telephone>): AbstractDictionaryImpl<Telephone>(list) {
+    override fun search(query: String): Telephone {
+        return list.first { it.phone == query }
+    }
+}
+
+data class Telephone(val phone: String)
